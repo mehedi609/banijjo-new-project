2641c8321d218fd759c8cea2a260fe20038bd25a
mehedi609 <35611282+mehedi609@users.noreply.github.com>
2020-07-06 22:40:05 +0600

    some prettier changes

--- pages/api/api/products.js
+++ pages/api/api/products.js
@@ -1,20 +1,19 @@
 export default (req, res) => {
-    res.statusCode = 200
-    let products = [
-        {
-            "product_name": "T-Shirt",
-            "product_price": "1500.00",
-            "product_url": "product full url",
-            "product_image": "product image full url",
-        },
-        {
-            "product_name": "T-Shirt-2",
-            "product_price": "1200.00",
-            "product_url": "product full url 2",
-            "product_image": "product image full url 2",
-        },
-    ]
-        
-    res.json({ products })
-  }
-  
+  res.statusCode = 200;
+  let products = [
+    {
+      product_name: 'T-Shirt',
+      product_price: '1500.00',
+      product_url: 'product full url',
+      product_image: 'product image full url',
+    },
+    {
+      product_name: 'T-Shirt-2',
+      product_price: '1200.00',
+      product_url: 'product full url 2',
+      product_image: 'product image full url 2',
+    },
+  ];
+
+  res.json({ products });
+};
