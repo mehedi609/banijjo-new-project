9f2f903dd531fb147a20d07b346caeadad0dc07a
turzo267725 <turzo.dev@gmail.com>
2020-07-06 15:43:37 +0600

    policy pages fetch data from database

---
+++ pages/faq/index.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import BaseLayout from 'components/layout/base-layout';
+import Head from 'next/head';
+import { fetcher } from '../../utils/fetcher';
+
+const FAQ = (props) => {
+  const { policy } = props;
+  // console.log(policy);
+
+  return (
+    <BaseLayout>
+      <Head>
+        <title>Frequently Asked Questions</title>
+        <meta name="Frequently Asked Questions" content="page containing Frequently Asked Questions" />
+      </Head>
+
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12">
+            <h1 className="h3 font-weight-bold text-left mt-3">
+              Frequently Asked Questions
+            </h1>
+
+            <p className="text-justify mt-3">
+              {policy.length > 0 &&
+                policy.map(({ terms_and_conditions }) => terms_and_conditions)}              
+            </p>
+           
+          </div>
+        </div>
+      </div>
+    </BaseLayout>
+  );
+};
+
+export async function getStaticProps() {
+  const base = process.env.FRONTEND_SERVER_URL;
+
+  const policy = await fetcher(`${base}/api/getPolicy/Cookie%20Policy`);
+
+  return {
+    props: {
+      policy,
+    },
+  };
+}
+
+export default FAQ;
---
+++ pages/terms-conditions/index.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import BaseLayout from 'components/layout/base-layout';
+import Head from 'next/head';
+import { fetcher } from '../../utils/fetcher';
+
+const termsAndConditions = (props) => {
+  const { policy } = props;
+  // console.log(policy);
+
+  return (
+    <BaseLayout>
+      <Head>
+        <title>Terms and Conditions</title>
+        <meta name="Terms and Conditions" content="page containing Terms and Conditions" />
+      </Head>
+
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12">
+            <h1 className="h3 font-weight-bold text-left mt-3">
+              Terms and Conditions
+            </h1>
+
+            <p className="text-justify mt-3">
+              {policy.length > 0 &&
+                policy.map(({ terms_and_conditions }) => terms_and_conditions)}              
+            </p>
+           
+          </div>
+        </div>
+      </div>
+    </BaseLayout>
+  );
+};
+
+export async function getStaticProps() {
+  const base = process.env.FRONTEND_SERVER_URL;
+
+  const policy = await fetcher(`${base}/api/getPolicy/Terms%20and%20condition`);
+
+  return {
+    props: {
+      policy,
+    },
+  };
+}
+
+export default termsAndConditions;
