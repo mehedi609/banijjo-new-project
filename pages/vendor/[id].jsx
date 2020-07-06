3c6234841f90aa605686a0960ed1691d542a001d
mehedi609 <35611282+mehedi609@users.noreply.github.com>
2020-07-06 22:36:38 +0600

    more change in vendor page.

--- pages/vendor/[id].jsx
+++ pages/vendor/[id].jsx
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import Head from 'next/head';
 import BaseLayout from 'components/layout/base-layout';
 import { fetcher } from 'utils/fetcher';
