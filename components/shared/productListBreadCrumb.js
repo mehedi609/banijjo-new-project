0101e19ae13c36261f3496fd7f216b3e37ce79ae
mehedi609 <35611282+mehedi609@users.noreply.github.com>
2020-07-08 14:47:14 +0600

    some changes in productList.jsx

--- components/shared/productListBreadCrumb.js
+++ components/shared/productListBreadCrumb.js
@@ -1,27 +1,19 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import AppLink from './AppLink';
 
-const ProductListBreadCrumb = ({ breadcrumbs }) => {
-  //   const { id, category_name } = breadcrumbs.pop();
-  return (
-    <>
-      <ul className="breadcrumbProduct px-3 py-2 mb-2">
-        {breadcrumbs.length > 0 &&
-          breadcrumbs.map(({ id, category_name }) => (
-            <li className="d-inline-block" key={id}>
-              <AppLink href={`/productList/[id]`} as={`/productList/${id}`}>
-                <a>{category_name}</a>
-              </AppLink>
-            </li>
-          ))}
-      </ul>
-    </>
-  );
-};
-
-ProductListBreadCrumb.propTypes = {
-  breadcrumbs: PropTypes.array.isRequired,
-};
+const ProductListBreadCrumb = ({ breadcrumbs }) => (
+  <>
+    <ul className="breadcrumbProduct px-3 py-2 mb-2">
+      {breadcrumbs.length > 0 &&
+        breadcrumbs.map(({ id, category_name }) => (
+          <li className="d-inline-block" key={id}>
+            <AppLink href={`/productList/[id]`} as={`/productList/${id}`}>
+              <a className="text-primary">{category_name}</a>
+            </AppLink>
+          </li>
+        ))}
+    </ul>
+  </>
+);
 
 export default ProductListBreadCrumb;
