67b167c4c54b9e820be1480b9796f84dfb3ec5e0
mehedi609 <35611282+mehedi609@users.noreply.github.com>
2020-07-08 14:58:37 +0600

    merged done.

--- pages/auth/login-with-facebook.js
+++ pages/auth/login-with-facebook.js
@@ -1,19 +1,19 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 import FacebookLogin from 'react-facebook-login';
 // import FacebookLogin from "react-facebook-login/dist/facebook-login-render-props";
 
 const LoginWithFacebook = ({ submittedData }) => {
-  const responseFacebook = response => {
+  const responseFacebook = (response) => {
     console.log({ success: response });
     submittedData(response);
   };
 
-  const onFailure = response => {
-    alert("Log in Failed. Try again.");
+  const onFailure = (response) => {
+    alert('Log in Failed. Try again.');
   };
 
-  const banijjo_demo = "252755555904700";
-  const banijjo_com_bd = "1818637888366587";
+  const banijjo_demo = '252755555904700';
+  const banijjo_com_bd = '1818637888366587';
 
   return (
     <FacebookLogin
@@ -21,7 +21,7 @@ const LoginWithFacebook = ({ submittedData }) => {
       fields="name,email,picture"
       callback={responseFacebook}
       onFailure={onFailure}
-      render={renderProps => (
+      render={(renderProps) => (
         <button
           onClick={renderProps.onClick}
           className="loginBtn loginBtn--facebook"
